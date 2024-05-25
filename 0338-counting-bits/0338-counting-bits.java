3e7fc94d72e79fe0e329043b2c042bf41e3773bb
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-05-25 11:32:22 +0530

    Time: 2 ms (94.85%), Space: 46.6 MB (43.25%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] arr = new int[n+1];
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = Integer.bitCount(i);    
+        }
+        return arr;
+    }
+}
