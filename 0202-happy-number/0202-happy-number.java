cc3aa8ce5c1c9a543443b3a6671bb72e70cf5fac
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-06-16 07:33:05 +0530

    Time: 1 ms (68.35%), Space: 40.9 MB (18.04%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while(!seen.contains(n)){
+            seen.add(n);
+            int sum =0;
+            while(n!=0){
+                int temp = n%10;
+                sum += temp* temp;
+                n = n/10;
+            }
+            n = sum;
+        }
+        
+        return n==1;
+        
+    }
+}
