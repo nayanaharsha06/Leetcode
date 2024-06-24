fbc14e71fdb02e7a7f0bc0b5b29318d193ab5893
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-06-24 20:42:08 +0530

    Time: 2 ms (87.82%), Space: 55.6 MB (39.97%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,23 +1,36 @@
+import java.util.*;
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        
-        left[0] = 1;
-        for(int x = 1; x < n ; x++){
-            left[x] = left [x-1]*nums[x-1];
-        }
+        int l = nums.length;
+        int[] left = new int[l];
+        int[] right = new int[l];
 
-        right[n-1] = 1;
-        for(int y = n-2; y >-1 ; y--){
-            right[y] =  right[y+1]* nums[y+1];
-        }
-        
-        int[] ans = new int[n];
-        for(int i = 0 ; i<n; i++){
+        left[0]=1;
+        right[l-1] = 1;
+        for(int i = 1; i < l; i++){
+            left[i] = left[i-1] * nums[i-1];
+        } 
+        for(int j = l-2; j  >=0; j--){
+            right[j] = right[j+1] * nums[j+1];
+        }   
+        int[] ans = new int[l];
+        for(int i =0; i <l ; i++){
             ans[i] = left[i]*right[i];
         }
         return ans;
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        int[] p = Arrays.stream(input.split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        Solution sol = new Solution();
+        int[] result = sol.productExceptSelf(p);
+
+        System.out.println(Arrays.toString(result));
+        
     }
 }
