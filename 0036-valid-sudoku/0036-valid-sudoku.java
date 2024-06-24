747e779c5bb95bff147a4be28b6a7fafa481f95d
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-06-24 23:06:51 +0530

    Time: 15 ms (16.73%), Space: 44.8 MB (25.6%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,17 +1,42 @@
+//https://www.youtube.com/watch?v=dgLypIfi9sk
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> seen = new HashSet<>();
+        HashSet<String> a = new HashSet<>();
 
         for(int i = 0; i <9; i++){
             for(int j = 0; j<9 ; j++){
-                char curr_val = board[i][j];
-                if(curr_val != '.'){
-                    if(!seen.add(curr_val+"found in row"+ i) || !seen.add(curr_val +"found in column"+ j) || !seen.add(curr_val +"found in sub box"+ i/3 + "-"+ j/3)){
+                char num = board[i][j];
+                if(num != '.'){
+                    if( !a.add(num + "at row" + i) ||
+                       !a.add(num + "at col" + j) ||
+                       !a.add(num + "at box" + i /3 + "," + j/3)){
                         return false;
                     }
                 }
             }
         }return true;
-        
     }
 }
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char[][] matrix  = new char[9][9];
+        for(int i = 0; i< 9; i++){
+            String line = sc.nextLine();
+            for (int j = 0; j < 9; j++) {
+                matrix[i][j] = line.charAt(j);
+            }
+        }
+
+        Solution sol = new Solution();
+    }
+}
+
+
+/*add - returns a boolean , if we cannot add which means
+ we have duplicate , then it is false
+not of false is true ; so if statement is true hence inner thing will be excuted
+If add returns true (indicating the value was added), !true becomes false.
+If add returns false (indicating a duplicate), !false becomes true.
+*/
