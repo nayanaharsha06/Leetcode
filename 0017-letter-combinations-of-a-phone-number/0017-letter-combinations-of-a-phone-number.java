8cb6185699297b80d775f5ffa835381462ea2cc7
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-07-14 05:54:15 +0530

    Time: 6 ms (13.84%), Space: 42.1 MB (34.77%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,37 @@
+//https://www.youtube.com/watch?v=xrRGmnybBlc
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        Map<Character,String>  map =  getmap();
+        List<String> ans = new ArrayList<>();
+        if(digits.length() == 0) return ans;
+        solve(0,digits,"",ans, map);
+        return ans;
+
+    }
+
+    public void solve(int i , String digits,String s,List<String> ans, Map<Character,String> map){
+        if(i == digits.length()){
+            ans.add(s);
+            return;
+        }
+
+        for(char c : map.get(digits.charAt(i)).toCharArray()){
+            String temp = new String(s);
+            temp += c;
+            solve(i+1, digits, temp, ans, map);
+        }
+    }
+
+    public Map<Character,String> getmap(){
+        Map<Character,String>  map = new HashMap<>();
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
+        return map;
+    }
+}
