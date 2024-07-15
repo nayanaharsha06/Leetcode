69c8a163d517db4c0c590427cfc78881050fc0b0
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-07-15 22:40:31 +0530

    Time: 114 ms (92.82%), Space: 41.6 MB (40.47%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,42 +1,38 @@
-public class Solution {
+class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-
-        boolean[][] visited = new boolean[m][n];
-        boolean result = false;
+        char[] arr = word.toCharArray();
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    result = backtrack(board, word, visited, i, j, 0);
-                    if (result) return true;
+        for(int i =0; i< board.length; i++){
+            for(int j =0; j < board[0].length; j++){
+                if(board[i][j] == arr[0] &&  func(board,i,j,0,arr) ){
+                    return true;
                 }
             }
         }
         
-        return false;
-    }
+      return false;
+        }
     
-    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
-        if (index == word.length()) {
+    public boolean func(char[][] board, int i , int j, int k, char[] arr){
+        if (k == arr.length) {
             return true;
         }
-        
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != arr[k]) {
             return false;
+
         }
-        
-        visited[i][j] = true;
-        
-        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
-            backtrack(board, word, visited, i - 1, j, index + 1) ||
-            backtrack(board, word, visited, i, j + 1, index + 1) ||
-            backtrack(board, word, visited, i, j - 1, index + 1)) {
-            return true;
-        }
-        
-        visited[i][j] = false;
-        return false;
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        boolean found = func(board, i + 1, j, k + 1, arr) ||
+                        func(board, i - 1, j, k + 1, arr) ||
+                        func(board, i, j + 1, k + 1, arr) ||
+                        func(board, i, j - 1, k + 1, arr);
+
+        // unmark the board cell
+        board[i][j] = temp;
+
+        return found;
+
     }
 }
