c6b0ec8e68e885730b400689bd238b404e220e0f
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-07-16 15:23:04 +0530

    Time: 13 ms (71.22%), Space: 49 MB (12.6%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
+       for(int num : nums){
+        map.put(num,map.getOrDefault(num,0)+1);
+       }
 
-        PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>((a,b) -> b.getValue() - a.getValue());
-
+       PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>((a,b) ->  b.getValue() - a.getValue());
+        // Adding all entries from the map to the priority queue
         for(Map.Entry entry : map.entrySet()){
-            pq.add(entry);
+          pq.add(entry);
         }
         int[] o = new int[k];
         for(int i = 0; i < k ; i++){
@@ -24,9 +24,9 @@ class main{
         String input = sc.nextLine();
 
         int k = sc.nextInt();
-          int[] a = Arrays.stream(input.split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        int[] a = Arrays.stream(input.split("\\s+")).mapToInt(Integer::parseInt).toArray();
         Solution sol = new Solution();
-         int[] topK = sol.topKFrequent(a, k);
+        int[] topK = sol.topKFrequent(a, k);
         System.out.println(Arrays.toString(topK)); //withput arrays.toString method we will get hashcode anf not array values
     }
 }
