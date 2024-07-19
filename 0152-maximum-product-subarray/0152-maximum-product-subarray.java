12b40e6061fcaa0f2000da7b0cd4e9b5db23acfe
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-07-19 07:41:05 +0530

    Time: 2 ms (72.88%), Space: 44.7 MB (46.39%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,22 +1,22 @@
+//https://www.youtube.com/watch?v=Y6B-7ZctiW8
 class Solution {
     public int maxProduct(int[] nums) {
         int n = nums.length;
         long rightProduct = 1;
         long leftProduct = 1;
         long ans = nums[0];
-        
-        for (int i = 0; i < n; i++) {
-            // If leftProduct or rightProduct is 0, reset to 1
-            if (leftProduct == 0 || leftProduct < Integer.MIN_VALUE) leftProduct = 1;
-            if (rightProduct == 0 || rightProduct < Integer.MIN_VALUE) rightProduct = 1;
+
+        for(int i = 0; i < n; i++){
+            // if any of the leftProcuts or rightProduct is 0 then update it to 1
+            if (leftProduct == 0 || leftProduct < Integer.MIN_VALUE)  leftProduct = 1;
+            if (rightProduct == 0 || rightProduct < Integer.MIN_VALUE)  rightProduct = 1;
 
             leftProduct *= nums[i];
             rightProduct *= nums[n - 1 - i];
 
-            ans = Math.max(ans, Math.max(leftProduct, rightProduct));
+            ans = Math.max(ans,  Math.max(leftProduct, rightProduct));
         }
-        
-        // Cast to int before returning
-        return (int) ans;
+        return (int)ans;
     }
 }
+       
