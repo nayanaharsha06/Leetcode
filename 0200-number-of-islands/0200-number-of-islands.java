e7e1cff2556086278b0c7cd0557ea2d8e59ca5b6
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-07-24 15:17:18 +0530

    Time: 3 ms (84.91%), Space: 49.4 MB (56.73%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,27 @@
-//https://www.youtube.com/watch?v=aKHEdGX0NQo
-
 class Solution {
-    public void dfs(char[][] grid, int i,int j) {
-        if(i<0 || i >= grid.length || j<0 ||j>=grid[0].length || grid[i][j] == '0' ){
-            return;
-        }
-        grid[i][j] = '0';
-        dfs(grid, i+1 , j);
-        dfs(grid, i-1 , j);
-        dfs(grid, i , j+1);
-        dfs(grid, i , j-1);
-    }
-    public int numIslands(char[][] grid){
-        int count = 0 ;
-        for(int i = 0; i < grid.length;i++){
-            for(int j = 0;j <grid[0].length; j++){
+    int count = 0;
+    public int numIslands(char[][] grid) {
+        for(int i = 0; i < grid.length ; i++){
+            for(int j = 0 ; j < grid[0].length ; j++){
                 if(grid[i][j] == '1'){
-                    dfs(grid,i,j);
+                    
+                    func(grid, i ,j);
                     count++;
                 }
             }
         }
         return count;
+    }
+
+    public void func(char[][] grid, int i, int j){
+        if( i < 0 || j < 0 || j >= grid[0].length || i >= grid.length || grid[i][j] == '0'){
+            return;
+        }
+        grid[i][j] = '0';
+        func(grid, i+1 ,j);
+        func(grid, i-1 , j);
+        func(grid, i , j+1);
+        func(grid, i , j-1);
+
     }
 }
