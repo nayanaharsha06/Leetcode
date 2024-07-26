a7a66ae64735293723563dc6582460f86f66835f
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-07-26 21:22:25 +0530

    Time: 8 ms (25.98%), Space: 44.1 MB (79.54%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -2,23 +2,22 @@
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int a_pointer = 0;
-        int b_pointer = 0;
+        int a = 0;
+        int b = 0;
         int max = 0;
-       
-        HashSet<Character> set =  new HashSet<>();
-        while(b_pointer < s.length()){
-            if(!set.contains(s.charAt(b_pointer))){
-                set.add(s.charAt(b_pointer));
-                b_pointer++;
-                max = Math.max(set.size(),max);
-            }else{
-                set.remove(s.charAt(a_pointer));
-                a_pointer++;
+
+        HashSet<Character> set = new HashSet<>();
+        while(b < s.length()){
+            if(!set.contains(s.charAt(b))){
+                set.add(s.charAt(b));
+                b++;
+                max = Math.max(max, set.size());
+            }else {
+                set.remove(s.charAt(a));
+                a++;
             }
         }
         return max;
     }
 }
-
 
