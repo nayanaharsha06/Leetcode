b85d6dae2d53a88e2e10907fcf37b6bb0a1d762d
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-07-29 07:43:14 +0530

    Time: 0 ms (100%), Space: 44.8 MB (5.89%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -1,3 +1,4 @@
+/*https://www.youtube.com/watch?v=IxXf_7LVGpg&t=29s*/
 /**
  * Definition for singly-linked list.
  * class ListNode {
@@ -9,24 +10,23 @@
  *     }
  * }
  */
+ //have to use slow and fast pointers only
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if(head == null || head.next == null){
             return false;
         }
-          
         ListNode slow = head;
         ListNode fast = head.next;
-        
+
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
             if(slow == fast){
                 return true;
             }
-            
-            
-
-        } return false;
+        }return false;
+          
+        
 }
 }
