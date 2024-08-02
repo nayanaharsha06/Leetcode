a8745addb534cf8dc2c49fc5fc3b2d6ef177336b
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-08-02 18:10:00 +0530

    Time: 143 ms (56.85%), Space: 119.4 MB (63.76%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.java
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.java
@@ -1,34 +1,46 @@
+//https://www.youtube.com/watch?v=qy3kjvlZLpc&t=608s
 class TimeMap {
-    HashMap<String, List<Pair<String, Integer>>> map;
+    HashMap<String, List<Pair<String,Integer>>> map;
+    //map could look like this: {"foo": [(bar, 1), (bar2, 4)]}
+
     public TimeMap() {
         map = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        map.putIfAbsent(key, new ArrayList<>());
-        map.get(key).add(new Pair(value,timestamp));
+        map.putIfAbsent(key,new ArrayList<>());
+        // If key is "foo", and there are no previous records, it creates a new empty list.
+        map.get(key).add(new Pair(value, timestamp));
+        //Adds the new pair to the list.
+        //Example: If the list was empty, after adding, it will contain [(bar, 1)].
     }
     
     public String get(String key, int timestamp) {
-        String res ="";
+        String res = "";
         if(map.containsKey(key)){
             List<Pair<String, Integer>> temp = map.get(key);
-            int left =0;
-            int right = temp.size()-1;
+            int left =0 ;
+            int right = temp.size() -1;
             while(left <= right){
-                int mid = left+ (right - left)/2;
+                int mid = (left+ right) /2;
                 if(temp.get(mid).getValue() <= timestamp){
                     res = temp.get(mid).getKey();
-                    left = mid+1;
+                    left = mid + 1;
                 }else{
-                    right = mid -1;
+                    right = mid - 1;
                 }
-            }
-        }return res;
-        
+
+            }return res;
+        }
+        return "";
     }
 }
 
+/*  tht list is itself acting like key and value pair 
+Each entry in the list associated with a key is a Pair, where:
+The first element (Pair.getKey()) is a timestamp (String).
+The second element (Pair.getValue()) is an integer value.
+ */
 /**
  * Your TimeMap object will be instantiated and called as such:
  * TimeMap obj = new TimeMap();
