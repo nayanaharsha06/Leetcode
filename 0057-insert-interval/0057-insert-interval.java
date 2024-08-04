f74b6679c0b6633f095135183f116dfd18d8240a
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-08-04 07:30:55 +0530

    Time: 1 ms (98.83%), Space: 44.6 MB (72.05%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.java
@@ -0,0 +1,29 @@
+//https://www.youtube.com/watch?v=BCZ_9clRkxo
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> result = new ArrayList<>();
+        int i = 0;
+        int n = intervals.length;
+        while(i < n && intervals[i][1] < newInterval[0]){
+            result.add(intervals[i]);
+            i++;
+        }
+         // Merge all overlapping intervals with newInterval
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+        result.add(newInterval);
+
+        // Add all intervals starting after newInterval ends
+        while (i < intervals.length) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+         
+    
+}
