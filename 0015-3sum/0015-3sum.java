a46266e0461ecc61aba6bff20d92d89487c7c50b
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-08-11 08:51:15 +0530

    Time: 829 ms (19.85%), Space: 52.2 MB (33.27%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -7,8 +7,8 @@ class Solution {
         for(int i = 0; i<nums.length;i++){
             int j = i+1;
             int k = nums.length-1;
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
+            while(j < k){
+                int sum = nums[i]+ nums[j] + nums[k];
                 if(sum == target){
                     s.add(Arrays.asList(nums[i],nums[j],nums[k]));
                     j++;
@@ -22,7 +22,5 @@ class Solution {
         }
         output.addAll(s);
         return output;
-
-        
     }
 }
