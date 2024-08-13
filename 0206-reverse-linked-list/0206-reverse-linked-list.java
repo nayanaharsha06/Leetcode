834352590e3e70c625f1ce52eec91ffd77604533
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-08-13 05:44:25 +0530

    Time: 0 ms (100%), Space: 42.3 MB (47.9%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -13,17 +13,57 @@ class Solution {
         if(head == null){
             return null;
         }
+
         ListNode curr = head;
         ListNode prev = null;
 
         while(curr != null){
             ListNode temp = curr.next;
             curr.next = prev;
-
             prev = curr;
             curr = temp;
         }
         return prev;
-        
     }
 }
+/*
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int x){
+        val = x;
+    }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+public class Main {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        // Print the original list
+        System.out.print("Original List: ");
+        printList(head);
+
+        // Reverse the linked list
+        Solution solution = new Solution();
+        ListNode reversedHead = solution.reverseList(head);
+
+        // Print the reversed list
+        System.out.print("Reversed List: ");
+        printList(reversedHead);
+    }
+
+    // Helper method to print the linked list
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}*/
