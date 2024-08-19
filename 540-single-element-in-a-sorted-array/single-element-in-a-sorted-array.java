b6c219dac1e550ac1ca6cea412019661d27c9913
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-08-20 05:21:38 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (72.84%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,9 +1,28 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-         int xor = 0;
-         for(int num : nums){
-            xor ^= num;
-         }
-         return xor;
+        // initilaize the left and right pointer
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l < r) {
+
+            // if mid is odd, decrement it to make it even
+            int mid = l + (r - l) / 2;
+            if (mid % 2 == 1)
+                mid--;
+
+            // if the elements at mid and mid + 1 are the same, 
+            // then the single element must
+            // appear after the midpoint
+            if (nums[mid] == nums[mid + 1]) {
+                l = mid + 2;
+            }
+            // otherwise, we must search for the 
+            // single element before the midpoint
+            else {
+                r = mid;
+            }
+        }
+        return nums[l];
     }
 }
