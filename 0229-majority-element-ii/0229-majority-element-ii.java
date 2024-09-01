60c728c4e755803b31effea8d764e0a294519ef4
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-09-01 09:18:29 +0530

    Time: 11 ms (42.71%), Space: 47.4 MB (88.46%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,27 +1,22 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        // Create a frequency map to store the count of each element
-        Map<Integer, Integer> elementCountMap = new HashMap<>();
-        
-        // Iterate through the input array to count element occurrences
-        for (int i = 0; i < nums.length; i++) {
-            elementCountMap.put(nums[i], elementCountMap.getOrDefault(nums[i], 0) + 1);
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0 ; i < nums.length ; i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
         }
-        
-        List<Integer> majorityElements = new ArrayList<>();
-        int threshold = nums.length / 3;
-        
-        // Iterate through the frequency map to identify majority elements
-        for (Map.Entry<Integer, Integer> entry : elementCountMap.entrySet()) {
-            int element = entry.getKey();
-            int count = entry.getValue();
-            
-            // Check if the element count is greater than the threshold
-            if (count > threshold) {
-                majorityElements.add(element);
+        List<Integer> arr = new ArrayList<>();
+        int threshold = nums.length/3;
+
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            int key = entry.getKey();
+            int val = entry.getValue();
+
+            if(val > threshold){
+                arr.add(key);
             }
         }
-        
-        return majorityElements;
+        return arr;
     }
 }
