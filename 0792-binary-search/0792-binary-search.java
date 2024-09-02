f155a0dd8558b25403f5357c8c3e0b6e29ea3094
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-09-02 11:44:36 +0530

    Time: 0 ms (100%), Space: 45.7 MB (47.34%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,30 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length -1;
-        while(low<=high){
-            int middle = (low+high)/2;
-            if(nums[middle] == target){
-                return middle;
-            }else if( nums[middle] < target){
-                low = middle+1;
-            }else{
-                high = middle - 1;
+        int i = 0;
+        int j = nums.length -  1;
+        while(i <= j){
+            int mid = (i+j)/2;
+            if(nums[mid] < target){
+                i = mid +1;
+            }else if(nums[mid] > target){
+                j = mid - 1;
+            }else if(nums[mid] == target){
+                return mid;
             }
-        }
+        }   
         return -1;
     }
 }
-
-class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        int[] p = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
-        int target = sc.nextInt();
-        Solution sol = new Solution();
-        int s = sol.search(p,target);
-        System.out.println(s);
-    }
-}
-
