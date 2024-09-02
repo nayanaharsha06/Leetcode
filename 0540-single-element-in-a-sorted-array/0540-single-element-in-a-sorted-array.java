392373fbcb3db80f8fd071625d3684ddcc3e6805
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-09-02 18:56:36 +0530

    Time: 0 ms (100%), Space: 50.4 MB (23.96%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,28 +1,20 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        // initilaize the left and right pointer
-        int l = 0;
-        int r = nums.length - 1;
-
-        while (l < r) {
-
-            // if mid is odd, decrement it to make it even
-            int mid = l + (r - l) / 2;
-            if (mid % 2 == 1)
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if (mid % 2 == 1) {
                 mid--;
-
-            // if the elements at mid and mid + 1 are the same, 
-            // then the single element must
-            // appear after the midpoint
+            }
             if (nums[mid] == nums[mid + 1]) {
-                l = mid + 2;
+                left = mid + 2;
+            } else {
+                // Otherwise, move the right pointer
+                right = mid;
             }
-            // otherwise, we must search for the 
-            // single element before the midpoint
-            else {
-                r = mid;
-            }
+
         }
-        return nums[l];
+         return nums[left];
     }
 }
