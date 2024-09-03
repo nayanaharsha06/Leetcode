274fae7c495e1e447a7e16e1817efe9488d19ebc
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-09-03 17:34:20 +0530

    Time: 0 ms (100%), Space: 41.7 MB (97.38%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int i = 0;
-        int j = matrix[0].length - 1;
-        while(i < matrix.length && j >= 0){
+        int j = matrix[0].length - 1; 
+         while(i < matrix.length && j >= 0){
             if(target == matrix[i][j]){
                 return true;
             }else if(target > matrix[i][j]){
@@ -13,23 +13,3 @@ class Solution {
         }return false;
     }
 }
-
-class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int row= sc.nextInt();
-        int col= sc.nextInt();
-        int[][] arr = new int[row][col];
-        for(int i = 0; i <arr.length ;i++){
-            for(int j =0; j < arr.length; j++){
-                arr[i][j] = sc.nextInt();
-            }
-        }
-        int target = sc.nextInt();
-       
-        Solution sol = new Solution();
-         boolean result = sol.searchMatrix(arr, target);
-        System.out.println(result);
-    }
-}
-
