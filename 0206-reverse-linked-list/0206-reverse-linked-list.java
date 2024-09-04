ff29753ed3e49d6baa506acfd0d14b3f7d68ad8d
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-09-04 20:08:43 +0530

    Time: 0 ms (100%), Space: 42.3 MB (54.48%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,63 +8,26 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-
-        // Recursively reverse the rest of the list
-        ListNode newHead = reverseList(head.next);
-
-        // Set the next node's next pointer to point back to the current node
-        head.next.next = head;
-
-        // Set the current node's next pointer to null (to avoid cycles)
-        head.next = null;
-
-        // Return the new head of the reversed list
-        return newHead;
-    }
-}
-/*
-class ListNode{
-    int val;
-    ListNode next;
-    ListNode(){}
-    ListNode(int x){
-        val = x;
-    }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-public class Main {
-    public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-
-        // Print the original list
-        System.out.print("Original List: ");
-        printList(head);
-
-        // Reverse the linked list
-        Solution solution = new Solution();
-        ListNode reversedHead = solution.reverseList(head);
-
-        // Print the reversed list
-        System.out.print("Reversed List: ");
-        printList(reversedHead);
+        ListNode temp = null;
+        ListNode temp1 = head;
+        ListNode temp2 = head.next;
+        return helper(temp,temp1,temp2);
     }
 
-    // Helper method to print the linked list
-    public static void printList(ListNode head) {
-        ListNode current = head;
-        while (current != null) {
-            System.out.print(current.val + " ");
-            current = current.next;
+    public ListNode helper(ListNode temp,ListNode temp1, ListNode temp2){
+        if(temp1 == null) return temp;
+        temp1.next = temp;
+        temp = temp1;
+        temp1 = temp2;
+        if(temp2 != null ){
+            temp2 = temp2.next;
         }
-        System.out.println();
+        return helper(temp,temp1,temp2);
     }
-}*/
+}
