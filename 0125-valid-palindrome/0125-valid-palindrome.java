7fb44ff7e4439ff2230ec00bb0d63e46badbf864
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-09-06 10:34:27 +0530

    Time: 18 ms (16.42%), Space: 48.2 MB (5.71%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,14 +2,14 @@ class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase().replaceAll(" ","");
         s = s.replaceAll("[^0-9a-zA-Z]","");
-        StringBuilder b = new StringBuilder(s);
-        StringBuilder a = new StringBuilder(s).reverse();
-        String b1 = b.toString();
-        String a1 = a.toString();
-        if(b1.equals(a1)){
-            return true;
-        }else{
-            return false;
-        }
+        
+        return func(s,0,s.length()-1);
+    }
+
+    private boolean func(String s, int start, int end){
+        if (start >= end) return true; 
+        if (s.charAt(start) != s.charAt(end)) return false;
+        return func(s, start+1, end-1);
     }
 }
+
