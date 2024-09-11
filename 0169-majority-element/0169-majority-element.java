a8793065ea1e402c8fd0e9a75a368a71c3642a52
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-09-11 17:24:15 +0530

    Time: 1 ms (99.79%), Space: 52.7 MB (66.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
 
-        HashMap<Integer,Integer> map = new HashMap<>();
-
-        for(int i  = 0 ; i < nums.length ; i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // Phase 1: Find the candidate
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            } else if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+            }
         }
-        
 
-        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>(
-            (a, b) -> b.getValue() - a.getValue()
-        );
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            maxHeap.add(entry);
-        }
-
-        return maxHeap.poll().getKey();
+        // Since the problem guarantees the majority element always exists,
+        // we return the candidate found.
+        return candidate;
     }
 }
