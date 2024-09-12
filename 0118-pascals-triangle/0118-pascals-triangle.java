9e0bb4232033ef155ce17389a55f149cc088a3bc
nayanaharsha06 <141167218+nayanaharsha06@users.noreply.github.com>
2024-09-12 07:39:25 +0530

    Time: 1 ms (82.27%), Space: 41.8 MB (64.54%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -3,25 +3,27 @@ class Solution {
         List<List<Integer>> arr = new ArrayList<>();
         if(numRows == 1){
             arr.add(new ArrayList<>(Arrays.asList(1)));
-            return arr;
+            return arr; 
         }
-
         arr.add(new ArrayList<>(Arrays.asList(1)));
         List<Integer> prev = new ArrayList<>(Arrays.asList(1,1));
         arr.add(prev);
 
-        for(int i = 3; i <= numRows ; i++){
+        for(int i = 3; i <=  numRows ;i++){
             List<Integer> next = new ArrayList<>();
             next.add(1);
-            for(int j = 0 ; j < prev.size()-1; j++){
-                int val = prev.get(j)+prev.get(j+1);
+            for(int j = 0 ; j < prev.size() -1 ; j++){
+                int val = prev.get(j) + prev.get(j+1);
                 next.add(val);
             }
             next.add(1);
             arr.add(next);
             prev = next;
         }
-        
         return arr;
     }
 }
+
+/*
+new ArrayList<>(1) creates an empty ArrayList with an initial capacity of 1, but it doesn't actually add the number 1 to the list.
+*/
